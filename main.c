c9b1d96e1665bfc31cb95f3008f71c92f5a49df4
Thiago <thiagolima1011@gmail.com>
2023-01-12 21:12:35 -0300

    Projeto de tabuada com print em tela

--- main.c
+++ main.c
@@ -5,10 +5,17 @@ int main()
 {
     FILE *arq = fopen("C:\\Users\\thiag\\Desktop\\TesteArquivo\\tabuada.txt","w");
 
-    int i, numero;
+    int i;
 
-    //printf("Informe a tabuada: ");
-    //scanf("%d", &numero);
+    for(int i = 1; i <= 10;i++ ){
+        printf("\n=======================\n");
+        printf("*** Tabuada de %d ***\n",i);
+        printf("=======================\n");
+        for(int j = 1; j <= 10;j++){
+            printf("%d x %d = %d\n",i, j, i * j);
+        }
+        printf("=======================\n");
+    }
 
     for(int i = 1; i <= 10;i++ ){
         fprintf(arq,"\n=======================\n");
